399fb465197b3c9635623b9ca9f35851a1979c4b
Mohamed Essam <mohamed.atres07@gmail.com>
2021-05-22 10:58:46 +0200

    Setup basic scripts & basic Camera && organize some folders

---
+++ Assets/Scripts/Game/GraphicsManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This Script will be responsable for controlling lights and post processing volumes during gameplay
+/// </summary>
+public class GraphicsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
