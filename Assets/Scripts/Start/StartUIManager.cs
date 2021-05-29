efe2f521817c0f40de1c4b88ce9a783f1c4e0382
Mohamed Essam <mohamed.atres07@gmail.com>
2021-05-29 18:36:08 +0200

    Start developing in start scene

---
+++ Assets/Scripts/Start/StartUIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+namespace SparkBallGame
+{
+    public class StartUIManager : MonoBehaviour
+    {
+        [SerializeField] PlayerData _playerData;
+
+        [Header("UI Elements")]
+        [SerializeField] TextMeshProUGUI _HighScoreText;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _HighScoreText.text = _playerData.PlayerScore.ToString();
+        }
+
+        public void StartGame()
+        {
+            Debug.Log("Start Game");
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
