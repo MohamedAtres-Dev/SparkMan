21f0ef58eb29649b0417537132b1dccdee40878b
Mohamed Essam <mohamed.atres07@gmail.com>
2021-06-08 17:02:33 +0200

    Importing Dotween

--- Assets/Scripts/Game/SaveManager.cs
+++ Assets/Scripts/Game/SaveManager.cs
@@ -8,9 +8,11 @@ using BayatGames.SaveGameFree.Serializers;
 
 namespace SparkBallGame
 {
-    public class SaveManager : MonoBehaviour
+    public class SaveManager : Singlton<SaveManager>
     {
         [SerializeField] PlayerData _playerData;
+        [SerializeField] List<SaveValue> _saveValues;
+
         private string saveIdentifier = "SavePlayerData";
         private string loadedData;
         private static ISaveGameSerializer m_Serializer = new SaveGameJsonSerializer();
@@ -30,16 +32,30 @@ namespace SparkBallGame
         private void SaveData()
         {
             //PlayerPrefs.SetString(saveIdentifier, JsonUtility.ToJson(_playerData));
-            SaveGame.Save(saveIdentifier, JsonUtility.ToJson(_playerData), true, m_EncodePassword, m_Serializer, m_Encoder, m_Encoding , SaveGamePath.PersistentDataPath) ;
-            Debug.Log("Save Data");
+            SaveGame.Save(saveIdentifier, JsonUtility.ToJson(_playerData), true, m_EncodePassword, m_Serializer, m_Encoder, m_Encoding, SaveGamePath.PersistentDataPath);
+            foreach (var val in _saveValues)
+            {
+                val.valueAsset.SaveValue(val.valueAsset.value, val.saveKey);
+            }
         }
 
         private void LoadData()
         {
-            Debug.Log("Load Data");
-            loadedData =  SaveGame.Load<string>(saveIdentifier , "" , true , m_EncodePassword , m_Serializer , m_Encoder , m_Encoding , SaveGamePath.PersistentDataPath);
+            foreach (var val in _saveValues)
+            {
+                val.valueAsset.value = val.valueAsset.LoadValue(val.saveKey);
+            }
+
+            loadedData = SaveGame.Load<string>(saveIdentifier, "", true, m_EncodePassword, m_Serializer, m_Encoder, m_Encoding, SaveGamePath.PersistentDataPath);
             JsonUtility.FromJsonOverwrite(loadedData, _playerData);
             //JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(saveIdentifier).ToString(), _playerData);
         }
+    }
+
+    [System.Serializable]
+    public class SaveValue
+    {
+        public ValueAsset<int> valueAsset;
+        public string saveKey;
     }
 }
